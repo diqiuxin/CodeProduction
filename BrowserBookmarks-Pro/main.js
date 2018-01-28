7feca4bdb0a5e10d99cb8ed6bdbf3008afeab0de
diqiuxin <501993589@qq.com>
2018-01-28 20:32:03 +0800

    main.js favicon  updata

--- BrowserBookmarks-Pro/main.js
+++ BrowserBookmarks-Pro/main.js
@@ -7,14 +7,35 @@ function init(){
     ["a","s","d","f","g","h","j","k","l"],
     ["z","x","c","v","b","n","m"],
   ];
-
+  
   // 按键对应的 URL 
   let hash = {
     "q": "qq.com",
     "w": "163.com",
-    "e": "sina.com.cn",
+    "e": "ele.me",
+    "r": "kuwo.cn",
+    "t": "twitter.com",
+    "y": "youtube.com",
+    "u": "ulinix.com",
+    "i": "ip.cn",
+    "o": "outlook.com",
+    "p": "kushubao.com",
+    "a": "aliyun.com",
+    "s": "sina.cn",
+    "d": "dytt8.net",
+    "f": "firefox.com.cn",
+    "g": "google.cn",
+    "h": "hao123.com",
+    "j": "juejin.im",
+    "k": "36kr.com",
+    "l": "le.com",
+    "z": "zhihu.com",
+    "x": "xiedaimala.com",
+    "c": "ce.cn",
+    "v": "vancl.com",
     "b": "baidu.com",
-    "t": "taobao.com"
+    "n": "nike.com",
+    "m": "mi.com"
   }
 
   // 获取本地的 localStore 数据
