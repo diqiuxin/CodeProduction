472c512c45f498d953b1b7810046fd05cfe16e5b
diqiuxin <501993589@qq.com>
2018-02-06 17:53:28 +0800

    文件说明 注释

--- DrawingBoard/server.js
+++ DrawingBoard/server.js
@@ -1,20 +1,25 @@
-var http = require("http");
-var url = require("url");
-var fs = require("fs");
-var path = require("path");
+/**
+ * 局域网调试 服务器代码
+ * 
+ */ 
+
+let http = require("http");
+let url = require("url");
+let fs = require("fs");
+let path = require("path");
 
 let IP = "192.168.43.253";
 let port = "8088";
 
 http.createServer(function(req,res){
 	//得到用户的路径
-	var pathname = url.parse(req.url).pathname;
+	let pathname = url.parse(req.url).pathname;
 	//默认首页
 	if(pathname == "/"){
 		pathname = "index.html";
 	}
 	//拓展名
-	var extname = path.extname(pathname);
+	let extname = path.extname(pathname);
 
 	//真的读取这个文件
 	fs.readFile("./" + pathname,function(err,data){
@@ -29,7 +34,7 @@ http.createServer(function(req,res){
 		//MIME类型，就是
 		//网页文件：  text/html
 		//jpg文件 :   image/jpg
-		var mime = getMime(extname);
+		let mime = getMime(extname);
 		res.writeHead(200,{"Content-type":mime});
 		res.end(data);
 	});
